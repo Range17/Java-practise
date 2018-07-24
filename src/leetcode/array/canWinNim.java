0594edc49ef9ff44b9b4aa67a3a3bdc2eadca7fd
Your Name <email@example.com>
2018-07-24 11:40:09 +0800

    Nim Game

---
+++ src/leetcode/array/canWinNim.java
@@ -0,0 +1,11 @@
+package leetcode.array;
+
+public class canWinNim {
+    public boolean canWinNim(int n) {
+        if(n%4==0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
