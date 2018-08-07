5c4fa25277daf0300b16a4f55f2cc5a1f047687b
Your Name <email@example.com>
2018-08-07 15:34:42 +0800

    设计哈希映射

---
+++ src/leetcode/array/MyHashMap.java
@@ -0,0 +1,25 @@
+package leetcode.array;
+
+import java.util.Arrays;
+
+public class MyHashMap {
+
+    int[] map = new int[100001];
+
+    public MyHashMap(){
+        Arrays.fill(map,-1);
+    }
+
+    public void put(int key, int value){
+        map[key] = value;
+    }
+
+    public int get(int key){
+        return map[key];
+    }
+
+    public void remove(int key){
+        map[key] = -1;
+    }
+
+}
